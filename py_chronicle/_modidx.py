26b010c74fb51398f36fe2f96bdf6413a01e6d00
andrie <apdevries@gmail.com>
2023-04-26 17:12:07 +0100

    some improvements

--- py_chronicle/_modidx.py
+++ py_chronicle/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://andrie.github.io',
                 'git_url': 'https://github.com/andrie/py-chronicle',
                 'lib_path': 'py_chronicle'},
-  'syms': {'py_chronicle.core': {'py_chronicle.core.read_chronicle': ('core.html#read_chronicle', 'py_chronicle/core.py')}}}
+  'syms': { 'py_chronicle.core': { 'py_chronicle.core.describe_measures': ('core.html#describe_measures', 'py_chronicle/core.py'),
+                                   'py_chronicle.core.get_metric_values': ('core.html#get_metric_values', 'py_chronicle/core.py'),
+                                   'py_chronicle.core.plot_metric_values': ('core.html#plot_metric_values', 'py_chronicle/core.py'),
+                                   'py_chronicle.core.read_chronicle': ('core.html#read_chronicle', 'py_chronicle/core.py')}}}
