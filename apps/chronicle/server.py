317b0ae0eab904533212c382d34d313a572a82c1
andrie <apdevries@gmail.com>
2023-05-07 16:47:10 +0100

    feat: improved metrics explorer with filter

--- apps/chronicle/server.py
+++ apps/chronicle/server.py
@@ -1,41 +1,64 @@
 from shiny import App, render, ui, req, reactive
-from shinywidgets import output_widget, render_widget, register_widget, reactive_read
+from shinywidgets import render_widget, register_widget, reactive_read
 import chronicle.core as chr
+from config import config_get
+# from metrics_explorer import metrics_explorer_server
 
 metrics_dsn = "./nbs/data"
 metrics_data = chr.scan_chronicle_metrics(metrics_dsn, "2023/04/03")
-
+    
 def server(input, output, session):
 
+    ### metrics server
     import ipydatagrid
-    dat = metrics_data.metrics.describe()
-    metrics_grid = ipydatagrid.DataGrid(dat, 
-        selection_mode = "row",
-        layout = {"height": "16em"}, 
-        base_column_size=100,
-        column_widths = {"name": 150, "description": 300}
-    )
-    register_widget("metrics_grid", metrics_grid)
+    df = metrics_data.metrics.describe()
+
+    @reactive.Calc
+    def exp_dat():
+        # return df
+        f = input.exp_filter_names()
+        dat = df
+        if f != "":
+            dat = dat[dat.name.str.contains(f)]
+        f = input.exp_filter_service()
+        if f != "":
+            dat = dat[dat.service.str.contains(f)]
+        return dat
+    
+
+    @reactive.Calc
+    def exp_metrics_grid():
+        grid = ipydatagrid.DataGrid(
+            exp_dat(), 
+            selection_mode = "row",
+            layout = {"height": "16em"}, 
+            base_column_size=100,
+            column_widths = {"name": 150, "description": 300}
+        )
+        register_widget("exp_metrics_grid", grid, session = session)
+        return grid
+    # metrics_explorer_server(input, output, session, metrics_data)
+    
+    @reactive.Calc
+    def exp_extract_metric():
+        req(reactive_read(exp_metrics_grid(), "selections"))
+        z = reactive_read(exp_metrics_grid(), "selected_cell_values")
+        return z[1]
+
+    @output(id="exp_selected_metric")
+    @render.text
+    def _():
+        req(exp_extract_metric())
+        return str(exp_extract_metric())
+
+    @output(id="exp_metrics_plot")
+    @render_widget
+    def _():
+        return metrics_data.metrics.plot(exp_extract_metric(), exp_extract_metric())
+
 
     @output
     @render.ui
     @reactive.event(input.btn)
     def btn_value():
         return str(input.btn())
-
-    @reactive.Calc
-    def extract_metric():
-        req(reactive_read(metrics_grid, "selections"))
-        z = reactive_read(metrics_grid, "selected_cell_values")
-        return z[1]
-
-    @output
-    @render.text
-    def selected_metric():
-        req(extract_metric())
-        return str(extract_metric())
-    
-    @output(id="metrics_plot")
-    @render_widget
-    def _():
-        return metrics_data.metrics.plot(extract_metric(), extract_metric())
--- apps/chronicle/ui.py
+++ apps/chronicle/ui.py
@@ -1,35 +1,58 @@
 
-from shiny import App, render, ui, req, reactive
-import shiny
-from shinywidgets import output_widget, render_widget, register_widget, reactive_read
+from shiny import ui
 import shiny.experimental as x
+from shinywidgets import output_widget
 from faicons import icon_svg
+# from metrics_explorer import metrics_explorer_ui
+# import ipydatagrid
+
+style="border: 1px solid #999;"
 
 app_ui = ui.page_fluid(
     ui.panel_title("Chronicle metrics explorer"),
     ui.navset_tab_card(
-        # elements ----
+    
+        ### overview
         ui.nav(
             x.ui.value_box(
                 "Overview", "All the things"
             ), 
-            output_widget("metrics_grid"),
-            ui.output_text_verbatim("selected_metric"),
-            output_widget("metrics_plot")
-        ),
-        ui.nav(
-            x.ui.value_box(
-                "I got", 
-                "99",
-                "problems",
-                showcase=icon_svg("flask", height="60px")
+            ui.row(
+                ui.h2("Workbench"),
+                ui.column(6, "CPU", style=style),
+                ui.column(6, "Memory", style=style),
             ),
-            "To be developed"
+            ui.row(
+                ui.h2("Connect"),
+                ui.column(6, "CPU", style=style),
+                ui.column(6, "Memory", style=style),
+            ),
         ),
+
+        ### metrics explorer
+        ui.nav(
+            x.ui.card(
+                x.ui.card_header("Metrics explorer")
+                # height="250px"
+            ),
+            # metrics_explorer_ui(),
+            # ui.h2("Metrics explorer"),
+            ui.row(
+                ui.input_text("exp_filter_service", "Filter on service", placeholder="Enter text"),
+                ui.input_text("exp_filter_names", "Filter on name", placeholder="Enter text")
+            ),
+            output_widget("exp_metrics_grid"),
+            ui.output_text_verbatim("exp_selected_metric"),
+            output_widget("exp_metrics_plot"),
+            # metrics_explorer_ui()
+        ),
+
+        ### dynamic
         ui.nav(
             x.ui.value_box(
                 "Dynamic",
-                ui.output_ui("btn_value")
+                ui.output_ui("btn_value"),
+                showcase=icon_svg("flask", height="60px")
             ),
             ui.h2("To be developed"),
             ui.p("This is a placeholder for a future tab."),
